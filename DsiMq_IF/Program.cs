406f70a3aeb5a14a49870f66963b943b9040c021
강지훈 <catp11@naver.com>
2022-03-11 13:23:55 +0900

    Mutex 중복실행방지 추가, 매뉴얼, 인터페이스 정의서 추가

--- DsiMq_IF/Program.cs
+++ DsiMq_IF/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +15,27 @@ namespace DsiMq_IF
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new FrmMain());
+
+            bool bNew;
+
+            Mutex mutex = new Mutex(true, "DsiMq", out bNew);
+
+            if (bNew)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FrmMain());
+
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("이미 실행중인 프로세스가 있습니다.");
+                Application.Exit();
+            }
         }
     }
 }
